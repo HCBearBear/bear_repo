0f69c2dcc8038f30b4d9ad0f75d7e9a4fb4e1bba
HCBearBear <s710153@gmail.com>
2022-10-28 23:08:32 +0800

    first commit

---
+++ mywebapps/src/main/java/com/group_buy_photos/model/groupBuyPhotosVO.java
@@ -0,0 +1,9 @@
+package com.group_buy_photos.model;
+
+public class groupBuyPhotosVO implements java.io.Serializable{
+	
+	private Integer gbp_id;
+	private Integer gbitem_id;
+	private byte[] gb_photo;
+
+}
---
+++ mywebapps/src/main/java/test.java
@@ -0,0 +1,78 @@
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+public class test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	Connection con;
+	
+    public test() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	
+	
+	
+	
+	public void init() throws ServletException {
+		try {
+			Context ctx = new javax.naming.InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/David");
+			con = ds.getConnection();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void destroy() {
+		try {
+			if (con != null) con.close();
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
